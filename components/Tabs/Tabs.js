d81bc2e0cc6c828a7c112b13afc0d2abe22c7158
Tommy <tommykindle83@gmail.com>
2019-06-20 15:44:25 -0700

    debugged code tested working added two new tabs to the html file

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -23,7 +23,7 @@ class TabLink {
   select() {
      
     // Get all of the elements with the tabs-link class
-     const links = document.querySelector('.tabs-link')
+     const links = document.querySelectorAll('.tabs-link')
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
      Array.from(links).forEach(link => link.classList.remove('tabs-link-selected'));
@@ -45,7 +45,7 @@ class TabItem {
 
   select() {
     // Select all ".tabs-item" elements from the DOM
-     const items = document.querySelector('.tabs-item'); 
+     const items = document.querySelectorAll('.tabs-item'); 
 
     // Remove the class "tabs-item-selected" from each element
     Array.from(items).forEach(item => item.classList.remove('tabs-item-selected')); 
